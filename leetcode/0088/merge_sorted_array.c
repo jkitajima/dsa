733de69771ee113118db3b063809bc52978c94c4
J. Kitajima <jvt_kitajima@icloud.com>
2025-02-07 15:04:46 -0300

    resolved leetcode/0088

--- leetcode/0088/merge_sorted_array.c
+++ leetcode/0088/merge_sorted_array.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <limits.h>
 
 void printarr(int *arr, int arrlen, char *arrname) {
     printf("%s = [", arrname);
@@ -14,9 +13,6 @@ void printarr(int *arr, int arrlen, char *arrname) {
 }
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    // Print the first array (before merge)
-    printarr(nums1, m+n, "nums1 (before merge)");
-
     // Replace zeroed elements from the first array
     for (int i = 0; i < n; i++) {
         nums1[m+i] = nums2[i];
@@ -26,51 +22,73 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
     printarr(nums1, m+n, "nums1 (after merge)");
 
     // Reorder the elements to non-decreasing order
-    int counter = 1;
-    int max = INT_MIN;
-    loop:
-    printf("####   LOOP COUNTER %d   ####\n", counter);
-    // if (counter == 2) { return; }
-    for (int i = 0; i < m+n; i++) {
-        if (i+1 < m+n && nums1[i] > nums1[i+1]) {
-            int tmp = nums1[i+1];
-            nums1[i+1] = nums1[i];
-            nums1[i] = tmp;
-        }
-
-        if (i+1 == m+n) {
-            printf("[LAST_IDX] max = %d | curr = %d\n", max, nums1[i]);
-            if (nums1[i] != max) {
-                max = nums1[i];
-                printarr(nums1, m+n, "[WILL_LOOP_AGAIN] nums1");
-                counter++;
-                goto loop;
+    int idx = m+n;
+    while (idx >= 0) {
+        for (int i = 0; i < idx; i++) {
+            if (i+1 < idx && nums1[i] > nums1[i+1]) {
+                int tmp = nums1[i+1];
+                nums1[i+1] = nums1[i];
+                nums1[i] = tmp;
             }
         }
-
-        if (nums1[i] > max) {
-            printf("max = %d | curr = %d (swapping)\n", max, nums1[i]);
-            max = nums1[i];
-        } else {
-            printf("[DEBUG] max = %d | curr = %d\n", max, nums1[i]);
-        }
+        idx--;
     }
 
-    printarr(nums1, m+n, "nums1");
-
-    // printf("%s\n", nums1);
+    // Print the array after reordering
+    printarr(nums1, m+n, "nums1 (reordered)");
 }
 
 int main(void) {
-    int nums1[6] = {1, 2, 3, 0, 0, 0};
+    // Example 1
+    printf("[EXAMPLE 1]\n");
+    int ex1_nums1[6] = {1, 2, 3, 0, 0, 0};
     int m = 3;
 
-    int nums2[3] = {2, 0, 1};
+    int ex1_nums2[3] = {2, 5, 6};
     int n = 3;
 
-    int *nums1_ptr = nums1;
-    int *nums2_ptr = nums2;
-
+    int *nums1_ptr = ex1_nums1;
+    int *nums2_ptr = ex1_nums2;
+    
+    printarr(nums1_ptr, m+n, "nums1");
+    printarr(nums2_ptr, n, "nums2");
+    
     merge(nums1_ptr, m, m, nums2_ptr, n, n);
+    printf("\n");
+
+    // Example 2
+    printf("[EXAMPLE 2]\n");
+    int ex2_nums1[1] = {1};
+    m = 1;
+
+    int ex2_nums2[0] = {};
+    n = 0;
+
+    nums1_ptr = ex2_nums1;
+    nums2_ptr = ex2_nums2;
+    
+    printarr(nums1_ptr, m+n, "nums1");
+    printarr(nums2_ptr, n, "nums2");
+    
+    merge(nums1_ptr, m, m, nums2_ptr, n, n);
+    printf("\n");
+
+    // Example 3
+    printf("[EXAMPLE 3]\n");
+    int ex3_nums1[0] = {};
+    m = 0;
+
+    int ex3_nums2[1] = {1};
+    n = 1;
+
+    nums1_ptr = ex3_nums1;
+    nums2_ptr = ex3_nums2;
+    
+    printarr(nums1_ptr, m+n, "nums1");
+    printarr(nums2_ptr, n, "nums2");
+    
+    merge(nums1_ptr, m, m, nums2_ptr, n, n);
+    printf("\n");
+
     return 0;
 }
