a304a4c43bbd6c982ed5d57c0555e4bb0423a14f
J. Kitajima <jvt_kitajima@icloud.com>
2025-02-06 22:25:28 -0300

    array reordering not working; unused import; changed to custom sample

--- leetcode/0088/merge_sorted_array.c
+++ leetcode/0088/merge_sorted_array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 
 void printarr(int *arr, int arrlen, char *arrname) {
     printf("%s = [", arrname);
@@ -13,25 +14,58 @@ void printarr(int *arr, int arrlen, char *arrname) {
 }
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    // Print the first array (before replace)
-    printarr(nums1, m+n, "nums1");
+    // Print the first array (before merge)
+    printarr(nums1, m+n, "nums1 (before merge)");
 
     // Replace zeroed elements from the first array
     for (int i = 0; i < n; i++) {
         nums1[m+i] = nums2[i];
     }
 
-    // Print the first array (after replace)
+    // Print the first array (after merge)
+    printarr(nums1, m+n, "nums1 (after merge)");
+
+    // Reorder the elements to non-decreasing order
+    int counter = 1;
+    int max = INT_MIN;
+    loop:
+    printf("####   LOOP COUNTER %d   ####\n", counter);
+    // if (counter == 2) { return; }
+    for (int i = 0; i < m+n; i++) {
+        if (i+1 < m+n && nums1[i] > nums1[i+1]) {
+            int tmp = nums1[i+1];
+            nums1[i+1] = nums1[i];
+            nums1[i] = tmp;
+        }
+
+        if (i+1 == m+n) {
+            printf("[LAST_IDX] max = %d | curr = %d\n", max, nums1[i]);
+            if (nums1[i] != max) {
+                max = nums1[i];
+                printarr(nums1, m+n, "[WILL_LOOP_AGAIN] nums1");
+                counter++;
+                goto loop;
+            }
+        }
+
+        if (nums1[i] > max) {
+            printf("max = %d | curr = %d (swapping)\n", max, nums1[i]);
+            max = nums1[i];
+        } else {
+            printf("[DEBUG] max = %d | curr = %d\n", max, nums1[i]);
+        }
+    }
+
     printarr(nums1, m+n, "nums1");
 
     // printf("%s\n", nums1);
 }
 
 int main(void) {
-    int nums1[] = {1, 2, 3, 0, 0, 0};
+    int nums1[6] = {1, 2, 3, 0, 0, 0};
     int m = 3;
 
-    int nums2[] = {2, 5, 6};
+    int nums2[3] = {2, 0, 1};
     int n = 3;
 
     int *nums1_ptr = nums1;
